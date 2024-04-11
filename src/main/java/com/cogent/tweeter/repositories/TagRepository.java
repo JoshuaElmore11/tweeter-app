0dfe12104455e6edea63dd710f46e0d86dd60adc
Josh <JDElmore11@gmail.com>
2024-04-11 10:26:23 -0500

    payloads and project structure

---
+++ src/main/java/com/cogent/tweeter/repositories/TagRepository.java
@@ -0,0 +1,10 @@
+package com.cogent.tweeter.repositories;
+
+import com.cogent.tweeter.entities.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+
+}
---
+++ src/main/java/com/cogent/tweeter/services/impl/AuthServiceImpl.java
@@ -0,0 +1,9 @@
+package com.cogent.tweeter.services.impl;
+
+import com.cogent.tweeter.services.AuthService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+}
